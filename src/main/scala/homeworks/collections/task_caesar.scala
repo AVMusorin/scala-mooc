8f4ee876357c6e8c42df46fa8545bcfa375a5f89
Aleksandr Musorin <a.musorin@corp.mail.ru>
2021-01-11 01:02:11 +0300

    solution for 4 ex.

--- src/main/scala/homeworks/collections/task_caesar.scala
+++ src/main/scala/homeworks/collections/task_caesar.scala
@@ -1,8 +1,20 @@
 package homeworks.collections
 
-import homeworks.HomeworksUtils.TaskSyntax
-
 object task_caesar {
+
+  /**
+   * Full alphabet
+   * Elements should be orders by Chat.toInt
+   */
+  private final val ALPHABET = 'A' to 'Z'
+  /**
+   * Alphabet capacity
+   */
+  private final val ALPHABET_LENGTH = ALPHABET.length
+  /**
+   * Offset from 0 to index of first element from [[ALPHABET]]
+   */
+  private final val ALPHABET_OFFSET = 'A' - 1
 
   /**
    * В данном задании Вам предлагается реализовать функции,
@@ -17,15 +29,31 @@ object task_caesar {
    * @param offset сдвиг вперёд по алфавиту
    * @return зашифрованное слово
    */
-  def encrypt(word: String, offset: Int): String =
-    task"Реализуйте метод `encrypt`"()
+  def encrypt(word: String, offset: Int): String = {
+    def getEncryptChar(c: Char, offset: Int): Char = {
+      val indexWithoutBaseOffset = c - ALPHABET_OFFSET
+      val newIndex = indexWithoutBaseOffset + offset % ALPHABET_LENGTH
+      // return to start of alphabet if index greater then alphabet length
+      (newIndex % ALPHABET_LENGTH + ALPHABET_OFFSET).toChar
+    }
+    word.foldLeft(new StringBuilder) {
+      case (builder, char) => builder += getEncryptChar(char, offset)
+    }.toString()
+  }
 
   /**
    * @param cipher шифр, который необходимо расшифровать
    * @param offset сдвиг вперёд по алфавиту
    * @return расшифрованное слово
    */
-  def decrypt(cipher: String, offset: Int): String =
-    task"Реализуйте метод `decrypt`"()
-
+  def decrypt(cipher: String, offset: Int): String = {
+    def getDecryptChar(c: Char, offset: Int): Char = {
+      val indexWithoutBaseOffset = c - ALPHABET_OFFSET + ALPHABET_LENGTH
+      val newIndex = indexWithoutBaseOffset - offset % ALPHABET_LENGTH
+      (newIndex % ALPHABET_LENGTH + ALPHABET_OFFSET).toChar
+    }
+    cipher.foldLeft(new StringBuilder) {
+      case (builder, char) => builder += getDecryptChar(char, offset)
+    }.toString()
+  }
 }
--- src/main/scala/homeworks/collections/task_seq_riddle.scala
+++ src/main/scala/homeworks/collections/task_seq_riddle.scala
@@ -1,6 +1,6 @@
 package homeworks.collections
 
-import homeworks.HomeworksUtils.TaskSyntax
+import scala.annotation.tailrec
 
 object task_seq_riddle {
 
@@ -18,8 +18,23 @@ object task_seq_riddle {
    * 1. Реализуйте функцию генерирующую след последовательность из текущей
    * */
 
-  def nextLine(currentLine: List[Int]): List[Int] =
-    task"Реализуйте функцию генерирующую след последовательность из текущей"()
+  def nextLine(currentLine: List[Int]): List[Int] = {
+    @tailrec
+    def recValuesAcc(element: Int, tail: List[Int], seq: List[Int], currentAcc: Int = 1): List[Int] = {
+      tail match {
+        case x :: Nil if x == element => seq ++ List(currentAcc + 1, element)
+        case x :: Nil => seq ++ List(currentAcc, element) ++ List(1, x)
+        case x :: xs if x == element => recValuesAcc(x, xs, seq, currentAcc + 1)
+        case x :: xs => recValuesAcc(x, xs, seq ++ List(currentAcc, element))
+        case Nil => seq
+      }
+    }
+    currentLine match {
+      case Nil => List(1)
+      case h :: Nil => List(1, h)
+      case h :: tail => recValuesAcc(h, tail, List.empty[Int])
+    }
+  }
 
   /**
    * 2. Реализуйте ленивый список, который генерирует данную последовательность
@@ -29,6 +44,5 @@ object task_seq_riddle {
    *
    */
 
-  val funSeq: LazyList[List[Int]] =
-    task"Реализуйте ленивый список, который генерирует данную последовательность"()
+  val funSeq: LazyList[List[Int]] = List(1) #:: funSeq.map(nextLine)
 }
